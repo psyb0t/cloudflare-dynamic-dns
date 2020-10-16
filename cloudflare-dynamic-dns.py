1cca0ced10a55bd8aedb5550d0120866f8d572c7
Ciprian Mandache <ciprian@51k.eu>
2020-10-16 16:43:41 +0300

    execute job in child process + kill child on timeout

--- cloudflare-dynamic-dns.py
+++ cloudflare-dynamic-dns.py
@@ -6,6 +6,7 @@ import CloudFlare
 import re
 import time
 import json
+import os
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO,
                     format='%(asctime)s %(levelname)s %(message)s',
@@ -55,109 +56,143 @@ def get_domain_from_hostname(hostname):
     return domain, None
 
 
+def perform_job():
+    cloudflare = CloudFlare.CloudFlare(
+        email=CONFIG['cf_user_email_address'], token=CONFIG['cf_api_token'])
+
+    logging.info('getting public ip...')
+
+    public_ip, err = get_public_ip()
+    if err is not None:
+        logging.error(err)
+
+        return
+
+    logging.info('got public ip %s' % public_ip)
+
+    for hostname in CONFIG['hostnames']:
+        logging.info('processing hostname %s' % hostname)
+
+        cf_dns_record_data = {
+            'name': hostname,
+            'type': 'A',
+            'content': public_ip,
+            'proxied': True
+        }
+
+        domain, err = get_domain_from_hostname(hostname)
+        if err is not None:
+            logging.error(err)
+
+            continue
+
+        logging.info('getting zone for domain %s' % domain)
+
+        try:
+            zones = cloudflare.zones.get(params={'name': domain})
+        except Exception as e:
+            logging.error(
+                'exception on getting zones for domain %s - %s' % (domain, e.message))
+
+            continue
+
+        if not zones:
+            logging.error(
+                'no zone data for domain %s(hostname %s)' % (domain, hostname))
+
+            continue
+
+        zone = zones[0]
+
+        try:
+            zone_dns_a_records = cloudflare.zones.dns_records.get(
+                zone['id'], params={'name': hostname, 'type': 'A'})
+        except:
+            logging.error(
+                'exception on getting zone dns A records for domain %s - %s' % (domain, e.message))
+
+            continue
+
+        if zone_dns_a_records:
+            zone_dns_a_record = zone_dns_a_records[0]
+
+            if zone_dns_a_record['content'] == public_ip:
+                logging.info(
+                    'hostname %s already has the current public ip set' % hostname)
+
+                continue
+
+            logging.info(
+                'updating existing dns A record for %s' % hostname)
+
+            cf_dns_record_data['proxied'] = zone_dns_a_record['proxied']
+
+            try:
+                result = cloudflare.zones.dns_records.put(
+                    zone['id'], zone_dns_a_record['id'], data=cf_dns_record_data)
+
+                if not result:
+                    logging.error(
+                        'could not update existing dns A record for %s' % hostname)
+            except Exception as e:
+                logging.error(
+                    'exception on updating existing dns A record for %s - %s' % (hostname, e.message))
+
+            continue
+
+        logging.info('inserting new dns A record for %s' % hostname)
+
+        try:
+            result = cloudflare.zones.dns_records.post(
+                zone['id'], data=cf_dns_record_data)
+
+            if not result:
+                logging.error(
+                    'could not insert dns A record for %s' % hostname)
+        except Exception as e:
+            logging.error(
+                'exception on inserting dns A record for %s - %s' % (hostname, e.message))
+
+        continue
+
+
 def main():
     while True:
         logging.info('reading config file...')
 
         read_config()
 
-        cloudflare = CloudFlare.CloudFlare(
-            email=CONFIG['cf_user_email_address'], token=CONFIG['cf_api_token'])
+        pid = os.fork()
+        if pid == 0:
+            logging.info('child process start')
 
-        logging.info('getting public ip...')
+            perform_job()
 
-        public_ip, err = get_public_ip()
-        if err is not None:
-            logging.error(err)
+            logging.info('child process end')
 
-            return
+            os._exit(0)
+        else:
+            logging.info('created child process %d' % pid)
 
-        logging.info('got public ip %s' % public_ip)
-
-        for hostname in CONFIG['hostnames']:
-            logging.info('processing hostname %s' % hostname)
-
-            cf_dns_record_data = {
-                'name': hostname,
-                'type': 'A',
-                'content': public_ip,
-                'proxied': True
-            }
-
-            domain, err = get_domain_from_hostname(hostname)
-            if err is not None:
-                logging.error(err)
-
-                continue
-
-            logging.info('getting zone for domain %s' % domain)
-
-            try:
-                zones = cloudflare.zones.get(params={'name': domain})
-            except Exception as e:
-                logging.error(
-                    'exception on getting zones for domain %s - %s' % (domain, e.message))
-
-                continue
-
-            if not zones:
-                logging.error(
-                    'no zone data for domain %s(hostname %s)' % (domain, hostname))
-
-                continue
-
-            zone = zones[0]
-
-            try:
-                zone_dns_a_records = cloudflare.zones.dns_records.get(
-                    zone['id'], params={'name': hostname, 'type': 'A'})
-            except:
-                logging.error(
-                    'exception on getting zone dns A records for domain %s - %s' % (domain, e.message))
-
-                continue
-
-            if zone_dns_a_records:
-                zone_dns_a_record = zone_dns_a_records[0]
-
-                if zone_dns_a_record['content'] == public_ip:
+            wait_timeout = 30
+            wait_time = 0
+            while True:
+                if wait_time >= wait_timeout:
                     logging.info(
-                        'hostname %s already has the current public ip set' % hostname)
+                        'killing child process %d due to timeout' % pid)
 
-                    continue
+                    os.kill(pid, 9)
 
-                logging.info(
-                    'updating existing dns A record for %s' % hostname)
+                    os.waitpid(pid, 0)
 
-                cf_dns_record_data['proxied'] = zone_dns_a_record['proxied']
+                    break
 
-                try:
-                    result = cloudflare.zones.dns_records.put(
-                        zone['id'], zone_dns_a_record['id'], data=cf_dns_record_data)
+                wpid, wsts = os.waitpid(pid, os.WNOHANG)
+                if wpid != 0:
+                    break
 
-                    if not result:
-                        logging.error(
-                            'could not update existing dns A record for %s' % hostname)
-                except Exception as e:
-                    logging.error(
-                        'exception on updating existing dns A record for %s - %s' % (hostname, e.message))
-
-                continue
-
-            logging.info('inserting new dns A record for %s' % hostname)
-
-            try:
-                result = cloudflare.zones.dns_records.post(
-                    zone['id'], data=cf_dns_record_data)
-
-                if not result:
-                    logging.error(
-                        'could not insert dns A record for %s' % hostname)
-            except Exception as e:
-                logging.error(
-                    'exception on inserting dns A record for %s - %s' % (hostname, e.message))
-
-            continue
+                wait_time += 1
+                time.sleep(1)
 
         logging.info('sleeping for %d minutes' % CONFIG['sleep_time_minutes'])
 
